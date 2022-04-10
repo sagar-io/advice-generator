b3e3317969a01dd5034dce4321d43162c702ef0c
Sagar Sharma <tech.sagarsh@gmail.com>
2022-04-10 15:16:35 +0530

    Advice id functionality added

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const advice_box = document.getElementById('main-advice');
 const dice = document.getElementById('dice');
+
+const advice_id = document.getElementById('advice-id');
 let quote, num;
 
 updateQuote();
 
 function updateQuote() {
   fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
+    .then(response =>
+      response.json())
     .then(data => {
     quote = `${data.slip.advice}`;
     num = `${data.slip.id}`;
     advice_box.innerText = `"${quote}"`;
+    advice_id.innerText = `ADVICE #${num}`;
 });
-
 }
 
 dice.addEventListener('click', updateQuote);
